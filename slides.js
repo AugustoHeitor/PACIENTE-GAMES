a2f5999803f9906df35b39f8f9a3cf835d815c82
AugustoHeitor <auuheitor@gmail.com>
2022-01-23 23:31:58 -0200

    Versão 1.1

---
+++ slides.js
@@ -0,0 +1,21 @@
+let destaques = document.querySelector('#destaques')
+
+let r = 0
+
+setInterval(function() {
+    r = r+100
+
+    if(r === 100){
+        destaques.src="imagens/destaques/destaques2.png"
+    }
+
+    if(r === 200){
+        destaques.src="imagens/destaques/destaques3.png"
+    }
+
+    if(r === 300){
+        destaques.src="imagens/destaques/destaques1.png"
+        r = 0
+    }
+  }, 5000);
+
